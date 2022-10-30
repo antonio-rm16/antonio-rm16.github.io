96fce23cc79dc732e0eb640cd7b864a1a8f73692
antonio-rm16 <ca.riveros.m16@gmail.com>
2022-10-30 14:42:56 -0600

    Agregar script
    Agregar escript de java para que el botón funciones

---
+++ script.js
@@ -0,0 +1,8 @@
+//SELECCIONAR
+let boton = document.querySelector('button');
+//OBSERVAR
+boton.addEventListener('click',clicAct);
+//ACTUAR
+function clicAct(){
+  alert("Práctica realizada");
+}  
